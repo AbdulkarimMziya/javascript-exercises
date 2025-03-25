67bf6bd2d5f172d62fe0970648f5e9501c8f62b2
AbdulkarimMziya <abdulmziya@gmail.com>
2025-03-24 21:42:34 -0700

    Completed findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a,b) => {
+        const thisYear = new Date().getFullYear();
+        const lastGuy = (!a.yearOfDeath) ? thisYear - a.yearOfBirth : a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = (!b.yearOfDeath) ? thisYear - b.yearOfBirth : b.yearOfDeath - b.yearOfBirth;
+        
+        return (lastGuy > nextGuy) ? -1 : 1;
+    });
 
+    return oldest[0];
 };
 
 // Do not edit below this line
