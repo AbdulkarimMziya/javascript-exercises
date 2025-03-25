08a313a26ffb00bc2322a0688e5067a82649d526
AbdulkarimMziya <abdulmziya@gmail.com>
2025-03-24 17:25:34 -0700

    Fibonacci exercise complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(nth_term) {
+    let count
+    if(typeof nth_term !== "number"){
+        count = parseInt(nth_term);
+        if (isNaN(count)) return "OOPS";
+    }
+    else{
+        count = nth_term;
+    }
 
+    if(count < 0) return "OOPS";
+    if (count === 0) return 0;
+
+    let secondPrev = 0, firstPrev = 1;
+    for (let i = 2; i <= count; i++) {
+        let nextFibNum = secondPrev + firstPrev;
+        secondPrev = firstPrev;
+        firstPrev = nextFibNum;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.spec.js
+++ 10_fibonacci/fibonacci.spec.js
@@ -1,7 +1,7 @@
 const fibonacci = require('./fibonacci')
 
 describe('fibonacci', () => {
-  test('4th fibonacci number is 3', () => {
+  test.skip('4th fibonacci number is 3', () => {
     expect(fibonacci(4)).toBe(3);
   });
   test.skip('6th fibonacci number is 8', () => {
@@ -10,13 +10,13 @@ describe('fibonacci', () => {
   test.skip('10th fibonacci number is 55', () => {
     expect(fibonacci(10)).toBe(55);
   });
-  test.skip('15th fibonacci number is 610', () => {
+  test('15th fibonacci number is 610', () => {
     expect(fibonacci(15)).toBe(610);
   });
   test.skip('25th fibonacci number is 75025', () => {
     expect(fibonacci(25)).toBe(75025);
   });
-  test.skip('0th fibonacci number is 0', () => {
+  test('0th fibonacci number is 0', () => {
     expect(fibonacci(0)).toBe(0);
   });
   test.skip('doesn\'t accept negatives', () => {
@@ -28,7 +28,7 @@ describe('fibonacci', () => {
   test.skip('DOES accept strings', () => {
     expect(fibonacci("1")).toBe(1);
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(fibonacci("2")).toBe(1);
   });
   test.skip('DOES accept strings', () => {
